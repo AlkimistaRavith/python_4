5f7ff1e94a770b1a0d142564849e3181e9e2a069
AlkimistaRavith <ravith.d3@gmail.com>
2025-05-28 03:38:20 +0000

    avance ejercicio 1

--- conversiones.py
+++ conversiones.py
@@ -10,14 +10,14 @@ try:
         sol = float(input("Ingrese el valor de 1 peso chileno, en soles peruanos:  "))
         arg = float(input("Ingrese el valor de 1 peso chileno, en pesos argentinos:  "))
         dol = float(input("Ingrese el valor de 1 peso chileno, en dólares americanos:  "))
-        peso_cl = float(input("Ingrese el monto en pesos chilenos que desea tasar: $"))
+        peso_cl = int(input("Ingrese el monto en pesos chilenos que desea tasar: $"))
         validar = 1
 
     elif len(sys.argv) == 5 :
         sol = float(sys.argv[1])
-        peso_arg = float(sys.argv[2])
-        dolar = float(sys.argv[3])
-        peso_cl = float(sys.argv[4])
+        arg = float(sys.argv[2])
+        dol = float(sys.argv[3])
+        peso_cl = int(sys.argv[4])
         validar = 1
     #Si no se ingresan la  cantidad de argumentos necesarios, no se "valida" la operación
     else :
@@ -32,7 +32,7 @@ try:
     if validar == 0 :
         print("Error en el ingreso de datos. \nPuedes ingresar <python conversiones.py> para inicar, o \n <python conversoines.py [valor de $1 CLP en soles peruanos] [valor de $1 CLP en pesos argentinos] [valor de $1 CLP en dólares americanos] [cantidad de pesos chilenos (CLP) a tasar]")
     else :
-        print(f"Los {pesos_cl} pesos equivalen a: \n {sol_con} soles peruanos. \n {arg_conv} pesos argentinos. \n {dol_conv} dólares americanos.")
+        print(f"Los {peso_cl} pesos equivalen a: \n {sol_conv:.1f} soles peruanos. \n {arg_conv:.1f} pesos argentinos. \n {dol_conv:.1f} dólares americanos.")
 
 except ValueError:
     print("Has ingresado datos que no son numéricos. \nPuedes ingresar <python conversiones.py> para inicar, o \n <python conversoines.py [valor de $1 CLP en soles peruanos] [valor de $1 CLP en pesos argentinos] [valor de $1 CLP en dólares americanos] [cantidad de pesos chilenos (CLP) a tasar]")
