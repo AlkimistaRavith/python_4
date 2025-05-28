eea6ecd281eeae89b3106221ce91db2181e65280
AlkimistaRavith <ravith.d3@gmail.com>
2025-05-28 04:19:28 +0000

    ejercicio 2

--- word_count.py
+++ word_count.py
@@ -1,5 +1,10 @@
 
-with open(“texto.txt”, "r") as file:
-texto = file.read()
+with open("lorem_ipsum.txt", "r") as file:
+    texto = file.read()
 
-print(texto)
+caracteres_distintos = set(texto)
+print(f"Cantidad de caracteres distintos: {len(caracteres_distintos)}")
+
+palabras = texto.split()
+palabras_distintas = set(palabras)
+print(f"Cantidad de palabras distintas: {len(palabras_distintas)}")
